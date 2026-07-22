4f122179805c86644411b3906bb3e1ce1691f702
chanchal31366-code <chanchal31366@gmail.com>
2026-07-22 09:37:40 +0000

    prrttier

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+accountEmail="chanchal@gmail.com"
+accountPassword ="2121212"
+acountCity="pune"
+
+console.log(accountCity)
---
+++ 01_basics/02-dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";// treat all js cose as neer version
+
+//alert(3+3)
+
+let name =" chanchal"
+let age = 19
+let isloggedIn=false
+
+//Primitive datatype
+//number => 2 to power 53
+// bignit for more gretter number 
+//string => ""
+//boolean => true/false
+//null = standalone value
+//undefine =>
+//symbol => unique
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
